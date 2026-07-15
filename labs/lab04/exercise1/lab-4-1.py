854e8e46ed33ea60fd1c740ccf66f1e11f19222b
Hakimulator <luqman.ahhassan@gmail.com>
2026-07-15 11:56:41 +0800

    lab4 exercise 1

--- labs/lab04/exercise1/lab-4-1.py
+++ labs/lab04/exercise1/lab-4-1.py
@@ -1,9 +1,10 @@
 kwh = float(input())
 if kwh >= 100:
-    if kwh >= 200:
-        fee = 0.75
-    else:
+    if kwh > 100 and kwh <= 200:
         fee = 0.5
+    else:
+        if kwh > 200:
+            fee = 0.75
 else:
     fee = 0.3
 totalBill = fee * kwh
