8222916cc99a75e441d825ef1d7df5e20b10aeda
Hakimulator <luqman.ahhassan@gmail.com>
2026-09-02 11:26:51 +0800

    update

--- labs/lab06/exercise1/exercise1.py
+++ labs/lab06/exercise1/exercise1.py
@@ -1,4 +1,21 @@
-# Escape Characters Exercise
-# Print the receipt shown in the lab, using \n for new lines and \t for columns.
-# Calculate every total, subtotal, and tax in your code. Do not type the money
-# amounts in directly. Show every amount with exactly two decimal places.
+# Without \n - everything prints on one line
+coffee = 3.50
+muffin = 2.10
+water = 1.05
+quantity1 = 2
+quantity2 = 3
+quantity3 = 4
+totalcoffee = coffee*quantity1
+totalmuffin = muffin*quantity2
+totalwater =  water*quantity3
+subtotal = totalcoffee + totalmuffin + totalwater
+tax = subtotal*0.06
+total = subtotal+tax
+print("========== RECEIPT ==========")
+print("item\t price\tquantity total")
+print(f"Coffee\t {coffee} \t{quantity1} {totalcoffee}")
+print(f"Muffin\t {muffin} {quantity2} {totalmuffin}")
+print(f"Water\t {water}  {quantity3} {totalwater}")
+print(f"Subtotal \t {subtotal}")
+print(f"Tax(6%) \t {tax}")
+print(f"Total\t\t {total}")
